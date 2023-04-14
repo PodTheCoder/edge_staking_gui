19d247aff479607f9172d7f874ae99f3d7baeb7f
Pod <PodTheCoder@protonmail.com>
2023-04-15 00:56:01 +0200

    refactor - add last checked to requirements

--- src-tauri/src/check_requirements/mod.rs
+++ src-tauri/src/check_requirements/mod.rs
@@ -1,3 +1,5 @@
+use chrono::{Utc, DateTime};
+
 mod check_cpu;
 mod check_os;
 mod pretty_check_string;
@@ -5,7 +7,9 @@ mod pretty_check_string;
 // Runs all requirement checks
 pub fn main() -> String {
     // TODO: Make each string into an async event
-    let mut test_string = String::from("");
+    let mut result_string = String::from("");
+    // TODO: Create wrapper for last checked.
+    let dt: DateTime<Utc> = Utc::now();
 
     let mut cpu_arch = String::from("");
     match check_cpu::check_processor_architecture(){
@@ -30,14 +34,15 @@ pub fn main() -> String {
     }
 
     // Check System Infrastructure
-    test_string.push_str(&format!("OS: {} | ", &os_type));
-    test_string.push_str(&format!("Processor: {} | ", &cpu_arch));
+    result_string.push_str(&format!("OS: {} | ", &os_type));
+    result_string.push_str(&format!("Processor: {} | ", &cpu_arch));
     //TODO: Docker check. 
-    test_string.push_str("Did you have docker installed & running? \n");
+    result_string.push_str("DO you have Docker installed & running? Docker can take a while to start up.");
 
     println!("Checked all requirements.");
 
-    return test_string
+    result_string.push_str(&format!("Requirements last checked on: {} ", dt.format("%d %B %Y %H:%M:%S %Z").to_string()));
+    return result_string
 
     // Check OS
     // Check Processor Arch
