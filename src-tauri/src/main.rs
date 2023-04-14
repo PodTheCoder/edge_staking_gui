65975bda5421011a9c38e4c632ee97b93fbd7abf
Pod <PodTheCoder@protonmail.com>
2023-04-15 00:10:51 +0200

    refactor - rename initialize_prereqs to check_requirements

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,14 +12,14 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-fn initialize_prereqs() -> String {
+fn check_requirements() -> String {
     return check_requirements::main()
 }
 
 //TODO: Add boolean for every phase. (eg. system requirements has been checked.)
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, initialize_prereqs])
+        .invoke_handler(tauri::generate_handler![greet, check_requirements])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Requirements.vue
+++ src/components/Requirements.vue
@@ -3,11 +3,10 @@ import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
 
 const Requirements_response = ref("");
-const name = ref("");
 
 async function check_requirements() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  Requirements_response.value = await invoke("initialize_prereqs");
+  Requirements_response.value = await invoke("check_requirements");
 }
 </script>
 
