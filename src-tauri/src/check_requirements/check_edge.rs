e14b51c82bf1b8dbe96f3d85ee642aa443a8a549
Pod <PodTheCoder@protonmail.com>
2023-04-18 12:16:39 +0200

    refactor - extract download file method

--- src-tauri/src/check_requirements/check_edge.rs
+++ src-tauri/src/check_requirements/check_edge.rs
@@ -8,7 +8,6 @@ use std::{fs, io};
 use crate::check_requirements::check_specifications::{get_os_info, get_processor_info};
 use crate::check_requirements::pretty_check_string;
 
-// TODO: Rework to Tauri API? https://tauri.app/v1/api/js/http/ . Add progress bar in GUI.
 /// Create an edge url based on url components
 pub fn create_edge_url(
     net: String,
@@ -35,23 +34,28 @@ pub fn create_edge_url(
 }
 
 /// Downloads checksum of latest edge binary for system
-pub fn get_edge_cli_checksum() -> Result<String, isahc::Error> {
-    // Send a GET request and wait for the response headers.
-    // Must be `mut` so we can read the response body.
-
+pub fn get_edge_cli_checksum() -> Result<String, String> {
     let checksum_url = get_edge_cli_checksum_url();
-    println!("Download Url: {}", checksum_url);
+    let download_path = String::from("checksum");
 
-    // eg. isahc::get("https://files.edge.network/cli/mainnet/linux/x64/latest/checksum")?;
-    let mut response = isahc::get(checksum_url)?;
+    match download_file(checksum_url.clone(), download_path.clone()) {
+        Ok(_) => {}
+        Err(err) => {
+            let error_message = err;
+            return Err(error_message);
+        }
+    }
 
-    // Print some basic info about the response to standard output.
-    println!("Status: {}", response.status());
-    println!("Headers: {:#?}", response.headers());
+    println!("Checksum download Url: {}", checksum_url);
 
-    // Read the response body as text into a string and print it.
-    // print!("{}", response.text()?);
-    let checksum = response.text()?; // Checksum is SHA256
+    let checksum: String;
+    match fs::read_to_string(download_path) {
+        Ok(checksum_str) => checksum = checksum_str, // Checksum is SHA256
+        Err(err) => {
+            let error_message = err.to_string();
+            return Err(error_message);
+        }
+    }
 
     return Ok(checksum);
 }
@@ -171,49 +175,10 @@ pub(crate) fn get_edge_cli() -> String {
     let cli_download_url = get_edge_cli_download_url();
     println!("Download Url: {}", cli_download_url);
 
-    // let mut response =
-    //     isahc::get("https://files.edge.network/cli/mainnet/linux/x64/latest/checksum")?;
-    let mut response;
-    match isahc::get(cli_download_url) {
-        Ok(successful_response) => response = successful_response,
-        Err(error_response) => {
-            let error_message = format!("Error while downloading CLI. Error = {}", error_response);
-            return error_message;
-        }
-    }
-
-    // Print some basic info about the response to standard output.
-    println!("Status: {}", response.status());
-    println!("Headers: {:#?}", response.headers());
-
-    // Read the response body as text into a string and print it.
-    // print!("{}", response.text()?);
-    // let checksum = response.text()?; // Checksum is SHA256
-
-    let edge_cli_bytes;
-    match response.bytes() {
-        Ok(converted_byte_vector) => edge_cli_bytes = converted_byte_vector,
-        Err(error_response) => {
-            let error_message = format!(
-                "Error while converting downloaded file to bytes. Error: {}",
-                error_response
-            );
-            return error_message;
-        }
-    }
-    println!("Writing file. Program may lag while downloading.");
-    let mut file;
-    match File::create(filename) {
-        Ok(ok_file) => file = ok_file,
-        Err(err) => {
-            let error_message = format!("Error creating File based on filename. Potential solution: Run the program as administrator. Error = {}", err);
-            return error_message;
-        }
-    }
-    match file.write_all(&edge_cli_bytes) {
+    match download_file(cli_download_url, filename) {
         Ok(_) => {}
         Err(err) => {
-            let error_message = format!("Unable to write file. Error = {}", err);
+            let error_message = String::from(err);
             return error_message;
         }
     }
@@ -231,4 +196,48 @@ pub(crate) fn get_edge_cli() -> String {
             return errormessage;
         }
     }
+}
+
+// TODO: Rework to use Tauri API? https://tauri.app/v1/api/js/http/ . Add progress bar in GUI.
+/// Download a file from a url to a local download path
+fn download_file(download_url: String, download_path: String) -> Result<(), String> {
+    let mut response;
+    match isahc::get(download_url) {
+        Ok(successful_response) => response = successful_response,
+        Err(error_response) => {
+            let error_message = format!("Error while downloading CLI. Error = {}", error_response);
+            return Err(error_message);
+        }
+    }
+    println!("Status: {}", response.status());
+    println!("Headers: {:#?}", response.headers());
+    let edge_cli_bytes;
+    match response.bytes() {
+        Ok(converted_byte_vector) => edge_cli_bytes = converted_byte_vector,
+        Err(error_response) => {
+            let error_message = format!(
+                "Error while converting downloaded file to bytes. Error: {}",
+                error_response
+            );
+            return Err(error_message);
+        }
+    }
+    println!("Writing file. Program may lag while downloading.");
+    let mut file;
+    match File::create(download_path) {
+        Ok(ok_file) => file = ok_file,
+        Err(err) => {
+            let error_message = format!("Error creating File based on filename. Potential solution: Run the program as administrator. Error = {}", err);
+            return Err(error_message);
+        }
+    }
+
+    match file.write_all(&edge_cli_bytes) {
+        Ok(_) => {}
+        Err(err) => {
+            let error_message = format!("Unable to write file. Error = {}", err);
+            return Err(error_message);
+        }
+    }
+    return Ok(());
 }
