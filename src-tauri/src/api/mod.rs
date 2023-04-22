b2ebf2546efe4acb131301856e26c8c69ba93e8f
Pod <PodTheCoder@protonmail.com>
2023-04-22 23:19:29 +0200

    refactor - split derivation into separate api submodule

--- src-tauri/src/api/mod.rs
+++ src-tauri/src/api/mod.rs
@@ -6,6 +6,8 @@ use crate::{
     BackendCommunicator,
 };
 
+pub mod derivation;
+
 /// Convenience function, download file from Edge Index API and conver it into a hashmap
 pub async fn api_json_query_to_hashmap(
     download_url: String,
@@ -381,93 +383,6 @@ pub async fn lookup_value_from_api_hashmap(
     let error_message = format!("Did not execute parse_api_hashmap_to_field_value loops.");
     log_and_emit(error_message.clone(), backend_communicator.clone());
     return Err(error_message);
-}
-
-/// Use the API to derive the stake token from node address
-pub async fn derive_stake_token_from_node_address(
-    node_address: String,
-    backend_communicator: BackendCommunicator,
-) -> Result<String, String> {
-    let api_hashmap;
-    match get_node_session_from_api(node_address.clone(), backend_communicator.clone()).await {
-        Ok(ok_hashmap) => {
-            api_hashmap = ok_hashmap;
-        }
-        Err(_) => {
-            let error_message = format!("Unable to derive stake.");
-            log_and_emit(error_message.clone(), backend_communicator.clone());
-            return Err(error_message);
-        }
-    }
-
-    let json_object_key = format!("node:stake");
-    match lookup_value_from_api_hashmap(api_hashmap, json_object_key, backend_communicator.clone())
-        .await
-    {
-        Ok(ok_stake_value) => {
-            let stake_value = ok_stake_value;
-            let ok_message = format!("Found stake value {}", stake_value.clone());
-            log_and_emit(ok_message, backend_communicator.clone());
-            return Ok(stake_value);
-        }
-        Err(_) => {
-            let error_message = format!("Could not lookup stake value.");
-            log_and_emit(error_message.clone(), backend_communicator.clone());
-            return Err(error_message);
-        }
-    }
-}
-
-/// Use the api to derive the wallet address from the node address
-pub async fn derive_wallet_address_from_node_address(
-    node_address: String,
-    backend_communicator: BackendCommunicator,
-) -> Result<String, String> {
-    // Derive stake from device address
-    let stake_token;
-    match derive_stake_token_from_node_address(node_address.clone(), backend_communicator.clone())
-        .await
-    {
-        Ok(ok_stake_token) => {
-            stake_token = ok_stake_token;
-        }
-        Err(_) => {
-            let error_message = format!("Could not derive stake inside derive_wallet_address.");
-            log_and_emit(error_message.clone(), backend_communicator.clone());
-            return Err(error_message);
-        }
-    }
-
-    // Derive wallet address from stake
-
-    // Get JSON
-    let api_hashmap;
-    match get_stake_info_from_api(stake_token.clone(), backend_communicator.clone()).await {
-        Ok(ok_api_hashmap) => {
-            api_hashmap = ok_api_hashmap;
-        }
-        Err(err_str) => {
-            return Err(err_str);
-        }
-    }
-
-    // Lookup Value
-    let json_object_key = format!("wallet");
-    match lookup_value_from_api_hashmap(api_hashmap, json_object_key, backend_communicator.clone())
-        .await
-    {
-        Ok(ok_wallet_address) => {
-            let ok_message = format!(
-                "Derived wallet address: {} from stake {} based on node address {}",
-                ok_wallet_address.clone(),
-                stake_token.clone(),
-                node_address.clone()
-            );
-            log_and_emit(ok_message.clone(), backend_communicator.clone());
-            return Ok(ok_wallet_address);
-        }
-        Err(err_str) => return Err(err_str),
-    }
 }
 
 // char::is_whitespace
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use api::{
-    derive_wallet_address_from_node_address, get_node_session_from_api,
-    lookup_value_from_api_hashmap,
-};
+use api::derivation::derive_wallet_address_from_node_address;
 use tauri::Window;
 use utility::{load_config, log_and_emit};
 
