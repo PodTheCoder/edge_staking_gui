a339e9baaa64fafca1caef49ed2993e686b060f9
Pod <PodTheCoder@protonmail.com>
2023-04-24 22:50:39 +0200

    refactor - remove unused function get_node_snapshots_from_api

--- src-tauri/src/api/get.rs
+++ src-tauri/src/api/get.rs
@@ -38,39 +38,6 @@ pub async fn get_node_session_from_api(
     }
 }
 
-/// Query Index API for snapshots info based on node address
-pub async fn get_node_snapshots_from_api(
-    node_address: String, // eg. xe_7a65d81dC21E87d593aC30DFe0AcbC2622bbdAE8
-    backend_communicator: BackendCommunicator,
-) -> Result<HashMap<String, Value>, String> {
-    let base_download_url = format!("https://index.xe.network/snapshots/");
-    let download_url = format!("{}{}", base_download_url, node_address);
-
-    let download_path_str = format!(
-        "{}{}",
-        backend_communicator.data_dir,
-        format!("node_snapshot.json")
-    );
-
-    match json_to_api_hashmap(
-        download_url,
-        download_path_str,
-        backend_communicator.clone(),
-    )
-    .await
-    {
-        Ok(ok_hashmap) => {
-            let ok_hashmap_str = format!("{:?}", ok_hashmap);
-            log_and_emit(
-                format!("Node snapshots info {}", ok_hashmap_str.clone()),
-                backend_communicator.clone(),
-            );
-            return Ok(ok_hashmap);
-        }
-        Err(err_str) => return Err(err_str),
-    }
-}
-
 /// Query Index API for stake info based on stake token
 pub async fn get_stake_info_from_api(
     stake_token: String, // eg. 9d51f5129e9188ba9622163f06b34e51071be224209365ad367d1300979e0b0e
