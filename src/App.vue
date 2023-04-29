69204e4996bde62cda30dd8dba55ee25133eb26d
Pod <PodTheCoder@protonmail.com>
2023-04-29 21:32:23 +0200

    feat - add update node button

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Install_Edge_Cli from "./components/Install_Edge_Cli.vue";
-import Node_Control from "./components/Node_Control.vue";
+import Post_Initialization_Node_Control from "./components/Post_Initialization_Node_Control.vue";
 import Curstatus from "./components/Curstatus.vue";
 import Add_Device from "./components/Add_Device.vue";
 import Autostart from "./components/Autostart.vue";
@@ -233,7 +233,7 @@ load_and_set_initialization_status();
 
       <h2>Your Node Setup Is Complete!</h2>
       <p>From now on, this is your node control panel: </p>
-      <Node_Control />
+      <Post_Initialization_Node_Control />
 
     </div>
     <div class="step">
--- src/components/Post_Initialization_Node_Control.vue
+++ src/components/Post_Initialization_Node_Control.vue
@@ -14,10 +14,20 @@ async function device_start_emitter() {
     window: appWindow,
   });
 }
+
 async function device_stop_emitter() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   const appLocalDataDirPath = await appLocalDataDir();
   Node_Control_Response.value = await invoke("device_stop", {
+    datadir: appLocalDataDirPath,
+    window: appWindow,
+  });
+}
+
+async function update_cli_emitter() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  const appLocalDataDirPath = await appLocalDataDir();
+  Node_Control_Response.value = await invoke("update_edge_cli_from_frontend", {
     datadir: appLocalDataDirPath,
     window: appWindow,
   });
@@ -29,5 +39,6 @@ async function device_stop_emitter() {
   <div class="card">
     <button type="button" @click="device_start_emitter()">Start Node</button>
     <button type="button" @click="device_stop_emitter()">Stop Node</button>
+    <button type="button" @click="update_cli_emitter()">Update Node</button>
   </div>
 </template>
