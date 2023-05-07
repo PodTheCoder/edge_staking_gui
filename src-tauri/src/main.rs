49424bcf1b26a1e1d1b1999e160555f1b097d666
Pod <PodTheCoder@protonmail.com>
2023-05-07 12:50:16 +0200

    refactor : make .on_window_event more concise

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -360,12 +360,11 @@ fn main() {
             add_device_from_frontend
         ])
         .system_tray(tray)
-        .on_window_event(|event| match event.event() {
-            tauri::WindowEvent::CloseRequested { api, .. } => {
+        .on_window_event(|event| {
+            if let tauri::WindowEvent::CloseRequested { api, .. } = event.event() {
                 event.window().hide().unwrap();
                 api.prevent_close();
             }
-            _ => {}
         })
         .on_system_tray_event(|app, event| match event {
             SystemTrayEvent::DoubleClick {
