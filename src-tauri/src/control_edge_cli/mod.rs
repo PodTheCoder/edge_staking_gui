ce9bf04dd4af36f8dddf0bae0e2c90a74536a30b
Pod <PodTheCoderGitHub@proton.me>
2023-10-30 19:00:40 +0100

    fix: explictly define PATH/bin as command environment

--- src-tauri/src/control_edge_cli/mod.rs
+++ src-tauri/src/control_edge_cli/mod.rs
@@ -43,10 +43,14 @@ async fn command_edge_cli(
     let command = Command::new(bin_path)
         .args(arglist)
         .creation_flags(WINDOWS_CREATE_NO_WINDOW)
+        .env("PATH", "/bin")
         .output();
 
     #[cfg(not(target_os = "windows"))]
-    let command = Command::new(bin_path).args(arglist).output();
+    let command = Command::new(bin_path)
+        .args(arglist)
+        .env("PATH", "/bin")
+        .output();
 
     match command {
         Ok(command_completed_result) => {
